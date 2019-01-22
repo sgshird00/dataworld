4193c910a8c4fdd8e7007b7242f3f40be7ee91f1
sagar <sgshird00@gmail.com>
2019-01-22 22:46:38 +0530

    corrected the labels in the app file

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ tab2 = ttk.Frame(tab_control)
 tab_control.add(tab1, text='First')
 tab_control.add(tab2, text='Second')
 
-lbl1 = Label(tab1, text='label100', padx=5, pady=5)
+lbl1 = Label(tab1, text='label10', padx=5, pady=5)
 lbl1.grid(column=0, row=0)
-lbl2 = Label(tab2, text='label2000')
+lbl2 = Label(tab2, text='label20')
 lbl2.grid(column=0, row=0)
 
 tab_control.pack(expand=1, fill='both')
